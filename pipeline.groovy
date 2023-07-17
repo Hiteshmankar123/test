5accc1f3844ca4320d1a09e1f37944b8e76de5a5
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-17 17:48:22 +0530

    --- Auto Git Commit ---

--- pipeline.groovy
+++ pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 sh 'echo "Hello World"'
                 sh '''
-                    echo "Multiline shell new-step"
+                    echo "Multiline shell newstep"
                 '''
             }
         }
