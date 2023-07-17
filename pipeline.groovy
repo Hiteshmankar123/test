29174f12b17d15430ae9a389691b2c35eb9b8cfa
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-17 15:28:11 +0530

    --- Auto Git Commit ---

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,14 +1,9 @@
-pipeline {
-    agent none
+ipeline {
+    agent any
     stages {
-        stage('Example') {
-            agent any
-            options {
-                // Timeout counter starts BEFORE agent is allocated
-                timeout(time: 1, unit: 'SECONDS')
-            }
+        stage(‘Build’) {
             steps {
-                echo 'Hello World'
+                sh 'npm --version'
             }
         }
     }
