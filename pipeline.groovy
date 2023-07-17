199aeac5627b74abfb95f4971a8b444af730be53
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-17 15:29:01 +0530

    --- Auto Git Commit ---

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,13 @@
-ipeline {
+pipeline {
     agent any
     stages {
-        stage(‘Build’) {
+        stage('Build') {
             steps {
-                sh 'npm --version'
+                sh 'echo "Hello World"'
+                sh '''
+                    echo "Multiline shell steps"
+                    ls –lah
+                '''
             }
         }
     }
