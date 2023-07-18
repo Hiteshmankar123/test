dd901340d355fb02ec4b875a62b9ea4a60429c2b
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-18 16:52:22 +0530

    --- Auto Git Commit ---

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,14 +1,1 @@
-print("Hello World")
-print("Hitesh Mankar")
-name = "Hitesh Mankar"
-age = 2
-price = 0.0
-enjoying = True
 
-print(type(name))
-print(type(age))
-print(type(price))
-print(type(enjoying))
-
-Channel = "Train With Hitesh"
-print("My channel name is", Channel)
