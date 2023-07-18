00455a53c72965ece482aab181fa1f78137d99c4
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-18 15:06:57 +0530

    --- Auto Git Commit ---

---
+++ player_files/javascript(11).php
@@ -0,0 +1,8 @@
+
+/**
+ *
+ * @package     theme_eguru
+ * @copyright   2015 LMSACE Dev Team,lmsace.com
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+(function($){var img=$("#header").find('.avatar').find('img[src$="/u/f2"]');var src=img.attr('src');img.attr('src',src+'_white')})(jQuery)
