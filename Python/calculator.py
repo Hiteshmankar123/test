2699e99b0eb33e9a5d70390a41eb38b13d523013
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:10:00 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -2,15 +2,15 @@ num_1 = float(input("Enter a number:"))
 num_2 = float(input("Enter another number:"))
 choice = input("Enter the opeeration + - * /")
 elif choice == "+":
-     sum == num_1 + num_2
+     sum = num_1 + num_2
      print("The sum is", sum) 
-elif choice = "-":
+elif choice == "-":
      diff = num_1 - num_2
      print("The difference is ", diff)
-elif choice = "*"
+elif choice == "*"
      mul = num_1 * num_2
      print("The product is ", mul)
-elif choice = "/"   
+elif choice == "/"   
      div = num_1 / num_2
      print("The quotient is ", div)
 else: 
