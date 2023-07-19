7271b8e4b62259e499a597a35007b7d00f081d54
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 14:53:15 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,5 +1,5 @@
-num_1 = float(input("Enter a number:")
-num_2 = input("Enter another number:")
+num_1 = float(input("Enter a number:"))
+num_2 = floinput("Enter another number:"))
 
 sum = num_1 + num_2
 print(sum) 
