c0e405b36fe44e127bcf363563f60b301689bf6e
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:00:57 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -5,8 +5,8 @@ if choice = "+":
    sum = num_1 + num_2
    print("The sum is", sum) 
 if choice = "-":
-  diff = num_1 - num_2
-print("The difference is ", diff)
+   diff = num_1 - num_2
+   print("The difference is ", diff)
 mul = num_1 * num_2
 print("The product is ", mul)
 div = num_1 / num_2
