bbccf0a9d11c5c332d82fdb5c115b0cfce297ee8
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:00:17 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -4,7 +4,7 @@ choice = input("Enter the opeeration + - * /")
 if choice = "+":
    sum = num_1 + num_2
    print("The sum is", sum) 
-
+if 
 diff = num_1 - num_2
 print("The difference is ", diff)
 mul = num_1 * num_2
