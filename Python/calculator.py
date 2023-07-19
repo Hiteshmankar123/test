4b7d9c27731bf8a1f56497ccd8fe485d0c25b870
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:07:59 +0530

    --- Auto Git Commit ---

---
+++ Python/calculator.py
@@ -0,0 +1,17 @@
+num_1 = float(input("Enter a number:"))
+num_2 = float(input("Enter another number:"))
+choice = input("Enter the opeeration + - * /")
+elif choice = "+":
+     sum = num_1 + num_2
+     print("The sum is", sum) 
+elif choice = "-":
+   diff = num_1 - num_2
+   print("The difference is ", diff)
+elif choice = "*"
+   mul = num_1 * num_2
+   print("The product is ", mul)
+elif choice = "/"   
+    div = num_1 / num_2
+   print("The quotient is ", div)
+else: 
+   print("Invalid choice")  
