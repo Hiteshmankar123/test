0c43aafe27d6ee4657f5cc590b3d5c402be635a0
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 17:04:00 +0530

    --- Auto Git Commit ---

--- Python/data_structures.py
+++ Python/data_structures.py
@@ -6,7 +6,7 @@ list_of_num.append("Hitesh")
 print (type(list_of_num))
 
 
-list_of_cloud = [aws","azure","gcp","oracle"]
+list_of_cloud = ["aws","azure","gcp","oracle"]
 list_of_env = ["dev", "test","prod"]
 
 print(list_of_cloud)
