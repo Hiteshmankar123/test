9ae5e65b85924f94745d848fb55b55b03ca353b8
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:00:07 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -2,8 +2,8 @@ num_1 = float(input("Enter a number:"))
 num_2 = float(input("Enter another number:"))
 choice = input("Enter the opeeration + - * /")
 if choice = "+":
-    sum = num_1 + num_2
-print("The sum is", sum) 
+   sum = num_1 + num_2
+   print("The sum is", sum) 
 
 diff = num_1 - num_2
 print("The difference is ", diff)
