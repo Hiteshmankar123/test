3ff36128164119afcbd385fe2899da688452c7b3
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-19 15:14:42 +0530

    --- Auto Git Commit ---

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,7 +1,7 @@
 num_1 = float(input("Enter a number:"))
 num_2 = float(input("Enter another number:"))
 choice = input("Enter the opeeration + - * /")
-               elif choice == "+":
+elif choice == "+":
      sum = num_1 + num_2
      print("The sum is", sum) 
 elif choice == "-":
