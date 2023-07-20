94467ac2253aaecbfba116b6ffe233ffe1f58ed6
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-07-20 15:18:27 +0530

    --- Auto Git Commit ---

--- Python/package.py
+++ Python/package.py
@@ -4,6 +4,6 @@ import shutil
 print(os.getcwd())
 
 total, used,  free = shutil.disk_usage("/") 
-print("Total disk space is:", total)
+print("Total disk space is:", total )
 print("free disk space is:", used)
 print("free disk space is:", free)
